eed058d7966666e5b2161d97fab5063d0acff81c
Johnny Gan <ganjpaien@gmail.com>
2026-08-16 13:00:01 +0800

    Add initial iOS app project

---
+++ GJPLab/GJPLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  GJPLabApp.swift
+//  GJPLab
+//
+//  Created by Gan Jianping on 16/8/26.
+//
+
+import SwiftUI
+
+@main
+struct GJPLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GJPLabTests/GJPLabTests.swift
@@ -0,0 +1,19 @@
+//
+//  GJPLabTests.swift
+//  GJPLabTests
+//
+//  Created by Gan Jianping on 16/8/26.
+//
+
+import Testing
+@testable import GJPLab
+
+struct GJPLabTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
